cc47ff800aa3b1060ba5a24167104529379a57f8
Hea3veN <hea3venmc@gmail.com>
2016-07-03 18:12:58 -0300

    lots of love color scheme

--- render.py
+++ render.py
@@ -1,6 +1,7 @@
 #! /usr/bin/python
 
 banner = 'banner-lol'
+love_banner = 'banner-love'
 
 flairs = ['sinballoon', 'eunballoon', 'sourcelogo', 'sourcemusic', 'seasonofglass', 'flowerbud', 'snowflake']
 flair_custom_widths = {
@@ -28,7 +29,29 @@ colors = {
     'color3dk': '#0CAD94',
 
     'tabmenubg': 'rgba(12,173,148, 0.4)',
-    'tabmenubghl': 'rgba(12,173,148, 0.6)'
+    'tabmenubghl': 'rgba(12,173,148, 0.6)',
+
+# white
+    # 'love_color0': '#FFF8FD',
+    'love_color0': '#FFFFFF',
+# pink  #42acec
+    'love_color1': '#BA99AF',
+    'love_color1hl': '#48B3EE',
+    'love_color1dk': '#BA99AF',
+    'love_color1light': '#D9E7EE',
+# pink
+    'love_color2': '#BA99AF',
+    'love_color2light': '#FFFADC',
+    'love_color2dk': '#BA99AF',
+# green
+    'love_color3': '#a0ae97',
+    'love_color3hl': '#3CDDC4',
+    'love_color3light': '#BDDDD8',
+    'love_color3dk': '#ff7183',
+    # 'love_color3dk': '#BA99AF',
+
+    'love_tabmenubg': 'rgba(186,153,175, 0.4)',
+    'love_tabmenubghl': 'rgba(186,153,175, 0.6)'
 }
 
 flairs_style = ''
@@ -62,6 +85,7 @@ with open('style.css') as f:
     style = f.read()
 
 style = style.replace('/*BANNER_NAME*/', '%%' + banner + '%%')
+style = style.replace('/*LOVE_BANNER_NAME*/', '%%' + love_banner + '%%')
 style = style.replace('/*FLAIRS DATA*/', flairs_style)
 for color_name, color_value in colors.items():
     style = style.replace('/*' + color_name.upper() + '*/', color_value)
